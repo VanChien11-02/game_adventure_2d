cc64cd2091c0e9a076d5610319a37bb058145216
vchiendev <anhminhoppoa15@gmail.com>
2025-10-28 11:50:06 +0700

    change volume of sound

--- src/main/Sound.java
+++ src/main/Sound.java
@@ -3,11 +3,16 @@ package main;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import java.net.URL;
 
 public class Sound {
     Clip clip;
     URL[] soundURL = new URL[30]; //store sound
+    FloatControl fc;
+    int volumeScale = 2;
+    float volume;
+
     public Sound(){
         soundURL[0] = getClass().getResource("/sound/theme_game.wav");
         soundURL[1] = getClass().getResource("/sound/coin.wav");
@@ -29,6 +34,8 @@ public class Sound {
             AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
             clip = AudioSystem.getClip();
             clip.open(ais);
+            fc = (FloatControl)clip.getControl(FloatControl.Type.MASTER_GAIN); //change volume -80f to 6f
+            checkVolume();
         } catch (Exception e){
 
         }
@@ -41,5 +48,16 @@ public class Sound {
     }
     public void stop(){
         clip.stop();
+    }
+    public void checkVolume(){
+        switch(volumeScale){
+            case 0: volume = -80f; break;
+            case 1: volume = -20f; break;
+            case 2: volume = -12f; break;
+            case 3: volume = -5; break;
+            case 4: volume = 1f; break;
+            case 5: volume = 6f; break;
+        }
+        fc.setValue(volume);
     }
 }
